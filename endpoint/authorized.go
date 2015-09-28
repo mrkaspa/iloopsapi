d8aad17b234282fadaac8ad8479d3a853eb86eb8
Michel Perez <michel.ingesoft@gmail.com>
2015-09-27 22:36:02 -0500

    logs deleted

--- endpoint/authorized.go
+++ endpoint/authorized.go
@@ -1,66 +1,63 @@
 package endpoint
 
 import (
-  "fmt"
-  "net/http"
+	"net/http"
 
-  "bitbucket.org/kiloops/api/models"
+	"bitbucket.org/kiloops/api/models"
 
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 //Authorized middleware
 func Authorized() gin.HandlerFunc {
-  return func(c *gin.Context) {
-    authID := c.Request.Header.Get("AUTH_ID")
-    authToken := c.Request.Header.Get("AUTH_TOKEN")
-    fmt.Printf("authID >> %s", authID)
-    fmt.Printf("authToken >> %s", authToken)
-    if authID == "" || authToken == "" {
-      c.AbortWithStatus(http.StatusForbidden)
-    } else {
-      var user models.User
-      models.Gdb.Where("id = ? and token = ?", authID, authToken).First(&user)
-      if user.ID != 0 && user.Active {
-        c.Set("userSession", &user)
-        c.Next()
-      } else {
-        c.AbortWithStatus(http.StatusForbidden)
-      }
-    }
-  }
+	return func(c *gin.Context) {
+		authID := c.Request.Header.Get("AUTH_ID")
+		authToken := c.Request.Header.Get("AUTH_TOKEN")
+		if authID == "" || authToken == "" {
+			c.AbortWithStatus(http.StatusForbidden)
+		} else {
+			var user models.User
+			models.Gdb.Where("id = ? and token = ?", authID, authToken).First(&user)
+			if user.ID != 0 && user.Active {
+				c.Set("userSession", &user)
+				c.Next()
+			} else {
+				c.AbortWithStatus(http.StatusForbidden)
+			}
+		}
+	}
 }
 
 //AdminAccessToProject middleware
 func AdminAccessToProject() gin.HandlerFunc {
-  return func(c *gin.Context) {
-    user := userSession(c)
-    if project, err := models.FindProjectBySlug(c.Param("slug")); err == nil {
-      if user.HasAdminAccessTo(project.ID) {
-        c.Set("currentProject", project)
-        c.Next()
-      } else {
-        c.AbortWithStatus(http.StatusForbidden)
-      }
-    } else {
-      c.AbortWithStatus(http.StatusNotFound)
-    }
-  }
+	return func(c *gin.Context) {
+		user := userSession(c)
+		if project, err := models.FindProjectBySlug(c.Param("slug")); err == nil {
+			if user.HasAdminAccessTo(project.ID) {
+				c.Set("currentProject", project)
+				c.Next()
+			} else {
+				c.AbortWithStatus(http.StatusForbidden)
+			}
+		} else {
+			c.AbortWithStatus(http.StatusNotFound)
+		}
+	}
 }
 
 //WriteAccessToProject middleware
 func WriteAccessToProject() gin.HandlerFunc {
-  return func(c *gin.Context) {
-    user := userSession(c)
-    if project, err := models.FindProjectBySlug(c.Param("slug")); err == nil {
-      if user.HasWriteAccessTo(project.ID) {
-        c.Set("currentProject", project)
-        c.Next()
-      } else {
-        c.AbortWithStatus(http.StatusForbidden)
-      }
-    } else {
-      c.AbortWithStatus(http.StatusNotFound)
-    }
-  }
+	return func(c *gin.Context) {
+		user := userSession(c)
+		if project, err := models.FindProjectBySlug(c.Param("slug")); err == nil {
+			if user.HasWriteAccessTo(project.ID) {
+				c.Set("currentProject", project)
+				c.Next()
+			} else {
+				c.AbortWithStatus(http.StatusForbidden)
+			}
+		} else {
+			c.AbortWithStatus(http.StatusNotFound)
+		}
+	}
 }
